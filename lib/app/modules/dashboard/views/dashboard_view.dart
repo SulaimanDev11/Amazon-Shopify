264160a6a8bb0a77a2c4a61592e651b8de948e6f
Muhammad Sulaiman <sulaikhan12@gmail.com>
2022-02-14 23:49:23 +0500

    removed unused imports

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:amazone_shopify/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
